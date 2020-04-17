112c86ba65fb634f3e75dee09da5ad4ff566c328
SHREYA <shreyavaghasia99@gmail.com>
2020-04-17 16:21:08 -0400

    [SV] Added Data Types In Login Activity

--- app/src/main/java/com/shreya/c0777726_w2020_mad3125_fp/Activity/LoginActivity.java
+++ app/src/main/java/com/shreya/c0777726_w2020_mad3125_fp/Activity/LoginActivity.java
@@ -1,16 +1,31 @@
 package com.shreya.c0777726_w2020_mad3125_fp.Activity;
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.example.c0775696_w2020_mad3125_fp.R;
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 
-public class LoginActivity extends AppCompatActivity
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener
 {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-    }
+    private String username,password;
+    private Button ok;
+    private EditText editTextUsername,editTextPassword;
+    private CheckBox saveLoginCheckBox;
+    private SharedPreferences loginPreferences;
+    private SharedPreferences.Editor loginPrefsEditor;
+    private Boolean saveLogin;
+
 }
