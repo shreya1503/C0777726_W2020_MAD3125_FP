4c2f543b8eae957a0333b9e89b44e576057b5615
SHREYA <shreyavaghasia99@gmail.com>
2020-04-17 15:59:52 -0400

    [SV] Created Splash Activity

---
+++ app/src/main/java/com/shreya/c0777726_w2020_mad3125_fp/Activity/SplashActivity.java
@@ -0,0 +1,17 @@
+package com.shreya.c0777726_w2020_mad3125_fp.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity
+{
+
+    private static final android.R.attr R = ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
