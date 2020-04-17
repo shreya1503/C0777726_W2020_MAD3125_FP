a350b556804926bd0e91502a43d63b1717c4d969
SHREYA <shreyavaghasia99@gmail.com>
2020-04-17 15:18:31 -0400

    [SV] Initial commit

---
+++ app/src/main/java/com/shreya/c0777726_w2020_mad3125_fp/MainActivity.java
@@ -0,0 +1,16 @@
+package com.shreya.c0777726_w2020_mad3125_fp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
