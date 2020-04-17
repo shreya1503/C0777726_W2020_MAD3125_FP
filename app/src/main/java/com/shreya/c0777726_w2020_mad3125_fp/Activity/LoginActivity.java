8f1745924a2682cd214e3a11ae23625b5c19d735
SHREYA <shreyavaghasia99@gmail.com>
2020-04-17 16:44:40 -0400

    [SV] Added onCreate, onClick and goNext In Login Activity

--- app/src/main/java/com/shreya/c0777726_w2020_mad3125_fp/Activity/LoginActivity.java
+++ app/src/main/java/com/shreya/c0777726_w2020_mad3125_fp/Activity/LoginActivity.java
@@ -28,4 +28,76 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     private SharedPreferences.Editor loginPrefsEditor;
     private Boolean saveLogin;
 
+
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        ActionBar mActionBar = getSupportActionBar();
+        mActionBar.hide();
+
+        ok = (Button)findViewById(R.id.buttonLogin);
+        ok.setOnClickListener(this);
+        editTextUsername = (EditText)findViewById(R.id.editTextUsername);
+        editTextPassword = (EditText)findViewById(R.id.editTextPassword);
+        saveLoginCheckBox = (CheckBox)findViewById(R.id.saveLoginCheckBox);
+        loginPreferences = getSharedPreferences("loginPrefs", MODE_PRIVATE);
+        loginPrefsEditor = loginPreferences.edit();
+
+        saveLogin = loginPreferences.getBoolean("saveLogin", false);
+        if (saveLogin == true) {
+            editTextUsername.setText(loginPreferences.getString("username", ""));
+            editTextPassword.setText(loginPreferences.getString("password", ""));
+            saveLoginCheckBox.setChecked(true);
+        }
+
+    }
+    public void onClick(View view) {
+        if (view == ok) {
+            InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(editTextUsername.getWindowToken(), 0);
+
+            username = editTextUsername.getText().toString();
+            password = editTextPassword.getText().toString();
+
+            if (saveLoginCheckBox.isChecked())
+            {
+                loginPrefsEditor.putBoolean("saveLogin", true);
+                loginPrefsEditor.putString("username", username);
+                loginPrefsEditor.putString("password", password);
+                loginPrefsEditor.commit();
+                // doSomethingElse();
+            } else
+            {
+                loginPrefsEditor.clear();
+                loginPrefsEditor.commit();
+                //    doSomethingElse();
+            }
+            goNext();
+        }
+    }
+    public void goNext()
+    {
+        if (editTextUsername.getText().toString().equals("shreya99@mail.com") && (editTextPassword.getText().toString().equals("Shreya"))) {
+            startActivity(new Intent(LoginActivity.this, CustomerListActivity.class));
+            LoginActivity.this.finish();
+        }
+        else {
+            new MaterialAlertDialogBuilder(LoginActivity.this)
+                    .setTitle("Provided Password Or Username is Invalid")
+                    .setMessage("Please Check Values Before Entering")
+                    .setIcon(R.drawable.man)
+                    .setNegativeButton("OK", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    })
+                    .show();
+        }
+    }
+
+
 }
