121b44d4ce02719ed670c554937dfa67f3878157
SHREYA <shreyavaghasia99@gmail.com>
2020-04-17 16:05:35 -0400

    [SV] Updated Splash Activity

--- app/src/main/java/com/shreya/c0777726_w2020_mad3125_fp/Activity/SplashActivity.java
+++ app/src/main/java/com/shreya/c0777726_w2020_mad3125_fp/Activity/SplashActivity.java
@@ -1,17 +1,34 @@
 package com.shreya.c0777726_w2020_mad3125_fp.Activity;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.example.c0775696_w2020_mad3125_fp.R;
 
 public class SplashActivity extends AppCompatActivity
 {
 
-    private static final android.R.attr R = ;
+    private static int SPLASH_TIME_OUT = 5000;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        ActionBar mActionBar = getSupportActionBar();
+        mActionBar.hide();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashActivity.this, LoginActivity.class);
+                startActivity(i);
+                finish();
+
+            }
+        }, SPLASH_TIME_OUT);
     }
 }
