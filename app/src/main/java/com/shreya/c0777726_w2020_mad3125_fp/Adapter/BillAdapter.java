ad419d15934cee7ef1d6eabb21fcc0f802b17bff
SHREYA <shreyavaghasia99@gmail.com>
2020-04-18 00:42:24 -0400

    [SV] Created Bill Adapter

---
+++ app/src/main/java/com/shreya/c0777726_w2020_mad3125_fp/Adapter/BillAdapter.java
@@ -0,0 +1,16 @@
+package com.shreya.c0777726_w2020_mad3125_fp.Adapter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.shreya.c0777726_w2020_mad3125_fp.R;
+
+public class BillAdapter extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bill_adapter);
+    }
+}
