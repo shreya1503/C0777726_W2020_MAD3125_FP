f0096672133814cbb99152ece3d997ef52d56368
SHREYA <shreyavaghasia99@gmail.com>
2020-04-17 23:35:20 -0400

    [SV] Imported Customer List Activity

--- app/src/main/java/com/shreya/c0777726_w2020_mad3125_fp/Activity/CustomerListActivity.java
+++ app/src/main/java/com/shreya/c0777726_w2020_mad3125_fp/Activity/CustomerListActivity.java
@@ -1,10 +1,24 @@
 package com.shreya.c0777726_w2020_mad3125_fp.Activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.shreya.c0777726_w2020_mad3125_fp.Adapter.CustomerAdapter;
+import com.shreya.c0777726_w2020_mad3125_fp.Model.Customer;
 import com.shreya.c0777726_w2020_mad3125_fp.R;
+import com.shreya.c0777726_w2020_mad3125_fp.Util.DataSingelton;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
 
 public class CustomerListActivity extends AppCompatActivity
 {
