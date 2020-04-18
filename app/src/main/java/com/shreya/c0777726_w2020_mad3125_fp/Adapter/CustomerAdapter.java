4f4fa46eb56c341dd8a37246b6c8eebdeebd1d7c
SHREYA <shreyavaghasia99@gmail.com>
2020-04-18 01:05:46 -0400

    [SV] View Controller And Item Count Added In Customer Adapter

--- app/src/main/java/com/shreya/c0777726_w2020_mad3125_fp/Adapter/CustomerAdapter.java
+++ app/src/main/java/com/shreya/c0777726_w2020_mad3125_fp/Adapter/CustomerAdapter.java
@@ -1,16 +1,97 @@
 package com.shreya.c0777726_w2020_mad3125_fp.Adapter;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.c0775696_w2020_mad3125_fp.Activity.ShowBillDetailActivity;
+import com.example.c0775696_w2020_mad3125_fp.Model.Customer;
+import com.example.c0775696_w2020_mad3125_fp.R;
 import com.shreya.c0777726_w2020_mad3125_fp.R;
 
-public class CustomerAdapter extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class CustomerAdapter extends RecyclerView.Adapter<CustomerAdapter.CustomerViewHolder> // Add view holder
+{
+    ArrayList<Customer> customersArrayList;
+    public CustomerAdapter(ArrayList<Customer> customerArrayList)
+    {
+        this.customersArrayList = customerArrayList;
+    }
+
+    @NonNull
+    @Override
+    public CustomerViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType)
+    {
+
+        View mView = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_customer, parent, false);
+        CustomerViewHolder mCustomerViewHolder = new CustomerViewHolder(mView);
+        return mCustomerViewHolder;
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_customer_adapter);
+    public void onBindViewHolder(@NonNull final CustomerViewHolder holder, final int position)
+    {
+        Customer mCustomer = this.customersArrayList.get(position);
+        holder.txtName.setText(mCustomer.getFirstName());
+        holder.txtId.setText(mCustomer.getCustomerId());
+        holder.txtEmail.setText(mCustomer.getEmail());
+        holder.txtDob.setText(mCustomer.getDateOfBirth());
+        holder.txtLocation.setText(mCustomer.getLocation());
+        holder.txtGender.setText(mCustomer.getGender());
+        holder.imgAttractions.setImageResource(mCustomer.getCustomerImg());
+
+        holder.itemView.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                Customer customer = customersArrayList.get(position);
+                Intent mIntent = new Intent(holder.itemView.getContext(), ShowBillDetailActivity.class);
+                mIntent.putExtra("CustomerBills", customer);
+                holder.itemView.getContext().startActivity(mIntent);
+        /*Bundle bundle = new Bundle();
+        bundle.putSerializable("customerskey", customer);
+        attractionIntent.putExtras(bundle);
+        holder.itemView.getContext().startActivity(attractionIntent);*/
+
+                //Intent mIntent = new Intent(holder.itemView.getContext(), MainActivity.class);
+                //Add Parameter - Parcalble or serializable
+                //holder.itemView.getContext().startActivity(mIntent);
+
+            }
+        });
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return this.customersArrayList.size();
+    }
+
+    public class CustomerViewHolder extends RecyclerView.ViewHolder
+    {
+
+        TextView txtName,txtId,txtEmail,txtGender,txtDob,txtLocation;
+        ImageView imgAttractions;
+
+        public CustomerViewHolder(@NonNull View itemView)
+        {
+            super(itemView);
+            this.txtName = itemView.findViewById(R.id.txt_customer_name);
+            this.txtId = itemView.findViewById(R.id.txt_customer_id);
+            this.txtEmail = itemView.findViewById(R.id.txt_customer_email);
+            this.txtGender = itemView.findViewById(R.id.txt_customer_gender);
+            this.txtDob = itemView.findViewById(R.id.txt_customer_dob);
+            this.txtLocation = itemView.findViewById(R.id.txt_customer_location);
+
+            this.imgAttractions = itemView.findViewById(R.id.img_customer);
+        }
     }
 }
