9f2b6a852e376f547044614417371722a5915ba0
SHREYA <shreyavaghasia99@gmail.com>
2020-04-17 23:18:18 -0400

    [SV] Created Customer List Activity

---
+++ app/src/main/java/com/shreya/c0777726_w2020_mad3125_fp/Activity/CustomerListActivity.java
@@ -0,0 +1,18 @@
+package com.shreya.c0777726_w2020_mad3125_fp.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.shreya.c0777726_w2020_mad3125_fp.R;
+
+public class CustomerListActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer_list);
+    }
+}
