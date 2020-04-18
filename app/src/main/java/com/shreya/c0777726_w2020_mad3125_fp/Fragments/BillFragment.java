09eda3803c7a3c070171ffa39a8be18626422b34
SHREYA <shreyavaghasia99@gmail.com>
2020-04-18 01:46:05 -0400

    [SV] Added Fragments For Bills

--- app/src/main/java/com/shreya/c0777726_w2020_mad3125_fp/Fragments/BillFragment.java
+++ app/src/main/java/com/shreya/c0777726_w2020_mad3125_fp/Fragments/BillFragment.java
@@ -1,65 +1,94 @@
 package com.shreya.c0777726_w2020_mad3125_fp.Fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.shreya.c0777726_w2020_mad3125_fp.Model.Bill;
+import com.shreya.c0777726_w2020_mad3125_fp.Model.Customer;
 import com.shreya.c0777726_w2020_mad3125_fp.R;
+import com.shreya.c0777726_w2020_mad3125_fp.Util.UtilMethods;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link BillFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class BillFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+import java.util.ArrayList;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+public class BillFragment extends Fragment
+{
+    private TextView txtBillId;
+    private TextView txtBillDate;
+    private TextView txtBillAmount;
+    private ImageView imgBillType;
+    double totalAmount;
 
-    public BillFragment() {
+    FragmentManager mFragmentManager;
+    FragmentTransaction mFragmentTransaction;
+
+    public BillFragment()
+    {
         // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment BillFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static BillFragment newInstance(String param1, String param2) {
-        BillFragment fragment = new BillFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+                             Bundle savedInstanceState)
+    {
+
         return inflater.inflate(R.layout.fragment_bill, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState)
+    {
+        super.onViewCreated(view, savedInstanceState);
+        txtBillId = getView().findViewById(R.id.txtBillId);
+        txtBillDate = getView().findViewById(R.id.txtBillDate);
+        txtBillAmount = getView().findViewById(R.id.txtBillAmount);
+        imgBillType = getView().findViewById(R.id.imgBillType);
+
+        Intent mIntent = getActivity().getIntent();
+        Customer customerObj = mIntent.getParcelableExtra("CustomerBills");
+
+        // cust = (Customer) getActivity().getIntent().getExtras().getSerializable("billsKey");
+        ArrayList<Bill> bills = customerObj.getBills();
+        for(int j = 0; j< bills.size(); j++)
+        {
+            totalAmount += bills.get(j).getBillTotal();
+        }
+
+        for(int i =0; i <bills.size(); i++)
+        {
+            if(bills.get(i).getBillType().equals(Bill.BillType.Mobile))
+            {
+                txtBillId.setText(bills.get(i).getBillId());
+                imgBillType.setImageResource(R.drawable.mobileicon);
+                txtBillAmount.setText(UtilMethods.getInstance().doubleFormatter(bills.get(i).billCalculate()));
+                txtBillDate.setText(bills.get(i).getBillDate().toString());
+            }
+
+            if(bills.get(i).getBillType().equals(Bill.BillType.Hydro))
+            {
+                txtBillId.setText(bills.get(i).getBillId());
+                imgBillType.setImageResource(R.drawable.hydroicon);
+                txtBillAmount.setText(UtilMethods.getInstance().doubleFormatter(bills.get(i).billCalculate()));
+                txtBillDate.setText(bills.get(i).getBillDate().toString());
+            }
+
+            if(bills.get(i).getBillType().equals(Bill.BillType.Internet))
+            {
+                txtBillId.setText(bills.get(i).getBillId());
+                imgBillType.setImageResource(R.drawable.interneticon);
+                txtBillAmount.setText(UtilMethods.getInstance().doubleFormatter(bills.get(i).billCalculate()));
+                txtBillDate.setText(bills.get(i).getBillDate().toString());
+            }
+        }
     }
 }
