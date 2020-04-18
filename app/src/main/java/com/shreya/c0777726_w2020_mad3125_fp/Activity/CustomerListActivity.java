aa3adb8f3f509dfc5f1ea508e5677c103a0c4589
SHREYA <shreyavaghasia99@gmail.com>
2020-04-18 00:35:41 -0400

    [SV] Functions Created In Customer List Activity

--- app/src/main/java/com/shreya/c0777726_w2020_mad3125_fp/Activity/CustomerListActivity.java
+++ app/src/main/java/com/shreya/c0777726_w2020_mad3125_fp/Activity/CustomerListActivity.java
@@ -22,11 +22,64 @@ import java.util.HashMap;
 
 public class CustomerListActivity extends AppCompatActivity
 {
+    private RecyclerView rvCustomers;
+    private ArrayList customers;
+    private ArrayList customerArrayList;
+    private CustomerAdapter customerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer_list);
+        ActionBar mActionBar = getSupportActionBar();
+        mActionBar.setTitle("CUSTOMERS LIST");
+        rvCustomers =findViewById(R.id.rv_customer_list);
+
+        rvCustomers = findViewById(R.id.rv_customer_list);
+        populateCustomers();
+
+        customerAdapter = new CustomerAdapter(customers);
+        RecyclerView.LayoutManager mLinearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        rvCustomers.setLayoutManager(mLinearLayoutManager);
+        rvCustomers.setAdapter(customerAdapter);
+
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.mymenu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        int id = item.getItemId();
+        switch (id)
+        {
+            case R.id.btnLogout:
+                Intent logout = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(logout);
+                finish();
+                return true;
+            case R.id.btnAdd:
+                startActivity(new Intent(CustomerListActivity.this, AddNewCustomerActivity.class));
+                //CustomerListActivity.this.finish();
+        }
+        return true;
+    }
+
+    private void populateCustomers()
+    {
+        DataSingelton.getInstance().populateData();
+        customers = new ArrayList();
+        HashMap<String, Customer> customerHashMap = DataSingelton.getInstance().getCustomerMap();
+        Collection<Customer> demoValues = customerHashMap.values();
+        customerArrayList = new ArrayList<>(demoValues);
+        customers.addAll(customerArrayList);
+
+    }
+
 }
