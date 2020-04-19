b119ca2041bf9d6826c789029101f753649573c7
SHREYA <shreyavaghasia99@gmail.com>
2020-04-19 19:46:46 -0400

    [SV] Data Types Added In Show Bill Detail Activity

--- app/src/main/java/com/shreya/c0777726_w2020_mad3125_fp/Activity/ShowBillDetailActivity.java
+++ app/src/main/java/com/shreya/c0777726_w2020_mad3125_fp/Activity/ShowBillDetailActivity.java
@@ -1,14 +1,33 @@
 package com.shreya.c0777726_w2020_mad3125_fp.Activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class ShowBillDetailActivity extends AppCompatActivity {
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_show_bill_detail);
-    }
+import com.shreya.c0777726_w2020_mad3125_fp.Adapter.BillAdapter;
+import com.shreya.c0777726_w2020_mad3125_fp.Model.Bill;
+import com.shreya.c0777726_w2020_mad3125_fp.Model.Customer;
+import com.shreya.c0777726_w2020_mad3125_fp.R;
+import com.shreya.c0777726_w2020_mad3125_fp.Util.UtilMethods;
+
+import java.util.ArrayList;
+
+public class ShowBillDetailActivity extends AppCompatActivity
+{
+    private RecyclerView rvBillsList;
+    private ArrayList billsArrayList;
+    private BillAdapter billsAdapter;
+    private ImageView imgAddButton;
+    private TextView txtTotalAmount;
+    Customer customerObj;
+
 }
