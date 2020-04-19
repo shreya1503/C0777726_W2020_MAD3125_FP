6ea7c0a7b5491f5d7c5224c7489f273a62e5351e
SHREYA <shreyavaghasia99@gmail.com>
2020-04-18 23:12:02 -0400

    [SV] Updated Bills In Model

--- app/src/main/java/com/shreya/c0777726_w2020_mad3125_fp/Model/Bill.java
+++ app/src/main/java/com/shreya/c0777726_w2020_mad3125_fp/Model/Bill.java
@@ -16,4 +16,62 @@ public abstract class Bill implements Serializable
         Hydro,
         Internet,
     }
+
+    String billId;
+    String billDate;
+    BillType billType;
+    Double billTotal = 0.0;
+
+    Bill(String billId, String billDate, BillType billType)
+    {
+        this.billId = billId;
+        this.billDate = billDate;
+        this.billType = billType;
+    }
+
+    public Double billCalculate()
+    {
+        double dummyAmount = 0.0;
+        return dummyAmount;
+    }
+
+    public String getBillId()
+    {
+        return billId;
+    }
+
+    public void setBillId(String billId) {
+        this.billId = billId;
+    }
+
+    public String getBillDate()
+    {
+        return billDate;
+    }
+
+    public void setBillDate(String billDate)
+    {
+        this.billDate = billDate;
+    }
+
+    public BillType getBillType()
+    {
+        return billType;
+    }
+
+    public void setBillType(BillType billType)
+    {
+        this.billType = billType;
+    }
+
+    public Double getBillTotal()
+    {
+        return billTotal;
+    }
+
+    public void setBillTotal(Double billTotal)
+    {
+        this.billTotal = billTotal;
+    }
+
 }
