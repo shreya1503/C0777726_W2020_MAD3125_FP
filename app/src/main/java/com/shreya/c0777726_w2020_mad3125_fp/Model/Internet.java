58a9de61625a5c9562b8119e948290cf3dcce16d
SHREYA <shreyavaghasia99@gmail.com>
2020-04-18 23:52:37 -0400

    [SV] Added Constructor In Internet Model

--- app/src/main/java/com/shreya/c0777726_w2020_mad3125_fp/Model/Internet.java
+++ app/src/main/java/com/shreya/c0777726_w2020_mad3125_fp/Model/Internet.java
@@ -1,4 +1,16 @@
 package com.shreya.c0777726_w2020_mad3125_fp.Model;
 
-public class Internet {
+import java.time.LocalDate;
+
+public class Internet extends Bill
+{
+    private String providerName;
+    private Double gbUsed;
+
+    public Internet(String billId, String billDate, BillType billType, String providerName, Double gbUsed) {
+        super(billId, billDate, billType);
+        this.providerName = providerName;
+        this.gbUsed = gbUsed;
+        this.billTotal = billCalculate();
+    }
 }
