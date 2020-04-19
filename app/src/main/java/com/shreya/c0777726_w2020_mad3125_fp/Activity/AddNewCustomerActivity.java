41a25007344f4c8362647ef7580fc78755e96895
SHREYA <shreyavaghasia99@gmail.com>
2020-04-19 19:30:44 -0400

    [SV] Data Types Included In New Customer Activity

--- app/src/main/java/com/shreya/c0777726_w2020_mad3125_fp/Activity/AddNewCustomerActivity.java
+++ app/src/main/java/com/shreya/c0777726_w2020_mad3125_fp/Activity/AddNewCustomerActivity.java
@@ -1,14 +1,36 @@
 package com.shreya.c0777726_w2020_mad3125_fp.Activity;
 
+import android.app.DatePickerDialog;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.RadioButton;
+
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.shreya.c0777726_w2020_mad3125_fp.Model.Customer;
+import com.shreya.c0777726_w2020_mad3125_fp.R;
+import com.shreya.c0777726_w2020_mad3125_fp.Util.DataSingelton;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
-public class AddNewCustomerActivity extends AppCompatActivity {
+import java.util.Calendar;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add_new_customer);
-    }
+public class AddNewCustomerActivity extends AppCompatActivity
+{
+
+    private TextInputLayout edtCustomerId, edtFirstName, edtLastName, edtBirthDate, edtUsername;
+    private TextInputLayout edtEmail, edtPassword, edtLocation;
+    private RadioButton rdBtnMale, rdBtnFemale, rdBtnOther;
+    private TextInputEditText edtCustomerIdText, edtFirstNameText, edtLastNameText, edtBirthDateText;
+    private TextInputEditText edtUsernameText, edtEmailText, edtPasswordText, edtLocationText;
+    private Button btnAdd, btnClear;
+    private DatePickerDialog.OnDateSetListener mDateSetListener;
+
+
 }
