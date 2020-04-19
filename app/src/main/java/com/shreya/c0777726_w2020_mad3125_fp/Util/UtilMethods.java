df8614d82d4faf2c7227fc620627a1b8ac1fe459
SHREYA <shreyavaghasia99@gmail.com>
2020-04-19 17:31:35 -0400

    [SV] Double Formatter Added In Util Methods

--- app/src/main/java/com/shreya/c0777726_w2020_mad3125_fp/Util/UtilMethods.java
+++ app/src/main/java/com/shreya/c0777726_w2020_mad3125_fp/Util/UtilMethods.java
@@ -1,4 +1,18 @@
 package com.shreya.c0777726_w2020_mad3125_fp.Util;
 
-public class UtilMethods {
+public class UtilMethods
+{
+    private static UtilMethods repoObj = new UtilMethods();
+    public static UtilMethods getInstance() {
+        return repoObj;
+    }
+
+    private UtilMethods()
+    { }
+
+    public String doubleFormatter(Double d)
+    {
+        return String.format("$ " + "%,.2f", d);
+    }
+
 }
