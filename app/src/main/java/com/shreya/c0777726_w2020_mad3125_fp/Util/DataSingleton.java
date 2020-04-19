318764e86a901b56d44e7da222f3845d226b97f6
SHREYA <shreyavaghasia99@gmail.com>
2020-04-19 02:30:26 -0400

    [SV] Created DataSingleton In Util

---
+++ app/src/main/java/com/shreya/c0777726_w2020_mad3125_fp/Util/DataSingleton.java
@@ -0,0 +1,13 @@
+package com.shreya.c0777726_w2020_mad3125_fp.Util;
+
+import com.shreya.c0777726_w2020_mad3125_fp.Model.Bill;
+import com.shreya.c0777726_w2020_mad3125_fp.Model.Customer;
+import com.shreya.c0777726_w2020_mad3125_fp.Model.Hydro;
+import com.shreya.c0777726_w2020_mad3125_fp.Model.Internet;
+import com.shreya.c0777726_w2020_mad3125_fp.Model.Mobile;
+import com.shreya.c0777726_w2020_mad3125_fp.R;
+
+
+public class DataSingleton
+{
+}
