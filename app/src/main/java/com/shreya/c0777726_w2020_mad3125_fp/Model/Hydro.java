68797e42730837f4f105b75427eee3f60ae26c08
SHREYA <shreyavaghasia99@gmail.com>
2020-04-19 01:02:14 -0400

    [SV] Added Constructor In Hydro Model

--- app/src/main/java/com/shreya/c0777726_w2020_mad3125_fp/Model/Hydro.java
+++ app/src/main/java/com/shreya/c0777726_w2020_mad3125_fp/Model/Hydro.java
@@ -1,4 +1,15 @@
 package com.shreya.c0777726_w2020_mad3125_fp.Model;
 
-public class Hydro {
+public class Hydro extends Bill
+{
+    private String agencyName;
+    private Integer unitsUsed;
+
+    public Hydro(String billId, String billDate, BillType billType, String agencyName, Integer unitsUsed)
+    {
+        super(billId, billDate, billType);
+        this.agencyName = agencyName;
+        this.unitsUsed = unitsUsed;
+        this.billTotal = billCalculate();
+    }
 }
