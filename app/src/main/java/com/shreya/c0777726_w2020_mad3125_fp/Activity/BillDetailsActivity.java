e62c739c97b37e7c22e918d0312512271e169924
SHREYA <shreyavaghasia99@gmail.com>
2020-04-19 22:22:39 -0400

    [SV] Bill Details Activity Updated

--- app/src/main/java/com/shreya/c0777726_w2020_mad3125_fp/Activity/BillDetailsActivity.java
+++ app/src/main/java/com/shreya/c0777726_w2020_mad3125_fp/Activity/BillDetailsActivity.java
@@ -1,14 +1,16 @@
 package com.shreya.c0777726_w2020_mad3125_fp.Activity;
 
+import android.widget.ImageView;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
-import android.os.Bundle;
-
-public class BillDetailsActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_bill_details);
-    }
+public class BillDetailsActivity extends AppCompatActivity
+{
+    FragmentManager mFragmentManager;
+    FragmentTransaction mFragmentTransaction;
+    private TextView txtTotalAmount;
+    private ImageView imgAddButton;
 }
